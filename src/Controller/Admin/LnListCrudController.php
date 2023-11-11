f43b15486a9f37e49cc615a3b6c5a0d32c2f7c32
Esteban DECTOT <estebandectot@gmail.Com>
2023-11-11 16:58:03 +0100

    Début lien liste/lightnovels

--- src/Controller/Admin/LnListCrudController.php
+++ src/Controller/Admin/LnListCrudController.php
@@ -20,7 +20,33 @@ class LnListCrudController extends AbstractCrudController
     {
         return [
             TextField::new('title'),
-            AssociationField::new('isOwned')
+            AssociationField::new('isOwned'),
+            /*
+            AssociationField::new('lightnovels')
+                ->onlyOnForms()
+                // on ne souhaite pas gérer l'association entre les
+                // lightnovels et la LnList dès la création de la
+                // LnList
+                ->hideWhenCreating()
+                ->setTemplatePath('admin/fields/[inventaire]_lightnovels.html.twig')
+                // Ajout possible seulement pour des lightnovels qui
+                // appartiennent même propriétaire de l'[inventaire]
+                // que le User de la LnList
+                ->setQueryBuilder(
+                    function (QueryBuilder $queryBuilder) {
+                        // récupération de l'instance courante de LnList
+                        $currentLnList = $this->getContext()->getEntity()->getInstance();
+                        $User = $currentLnList->getUser();
+                        $memberId = $User->getId();
+                        // charge les seuls lightnovels dont le 'owner' de l'[inventaire] est le User de la galerie
+                        $queryBuilder->leftJoin('entity.[inventaire]', 'i')
+                            ->leftJoin('i.owner', 'm')
+                            ->andWhere('m.id = :member_id')
+                            ->setParameter('member_id', $memberId);
+                        return $queryBuilder;
+                    }
+                ),
+            */
         ];
     }
 
